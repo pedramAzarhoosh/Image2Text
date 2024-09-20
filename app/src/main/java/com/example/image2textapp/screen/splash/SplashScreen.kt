bad42f3ac619b3e4770f307049f04721e68e59c3
pedram <pedram5204@gmail.com>
2024-09-20 18:56:29 +0330

    Update splash screen

--- app/src/main/java/com/example/image2textapp/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/image2textapp/screen/splash/SplashScreen.kt
@@ -1,8 +1,13 @@
 package com.example.image2textapp.screen.splash
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
@@ -10,19 +15,28 @@ import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import com.example.image2textapp.navigation.Screen
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.example.image2textapp.R
 
 @Composable
 fun splashScreen(navController: NavHostController){
     splash()
-    navController.popBackStack()
-    navController.navigate(Screen.Home.route)
 }
 
 @Composable
 fun splash(){
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.background(Color.White)) {
-        val painterRes = painterResource(id = R.drawable.image_to_text_back)
-        Image(painter = painterRes, contentDescription = "splash picture")
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        , modifier = Modifier.background(Color.White)) {
+        Box(contentAlignment = Alignment.Center) {
+            val painterRes = painterResource(id = R.drawable.image_to_text_back)
+            val imageModifier = Modifier
+                .size(200.dp)
+
+            Image(painter = painterRes, contentDescription = "splash picture",
+                contentScale = ContentScale.Fit,
+                modifier = imageModifier)
+        }
     }
 }
